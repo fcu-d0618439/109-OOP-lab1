ef702164a21dd5af189bcc7c62dc6bf90fe2258c
user <user@EF02>
2020-09-29 09:40:22 +0800

    change my name

--- src/hw1/hw1.java
+++ src/hw1/hw1.java
@@ -2,7 +2,7 @@ package hw1;
 
 public class hw1 {
 	public static void main(String[] args) {
-		System.out.println("I'am ½²®¦´­");
+		System.out.println("I'am Alen Chua");
 		System.out.println("My nid is D0618439");
 		System.out.println("This is lab 1 hw");
 
