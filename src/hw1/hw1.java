a1134a05c3716846e61922804fad1eb4d8488aca
user <user@EF02>
2020-09-29 09:35:50 +0800

    commit init

---
+++ src/hw1/hw1.java
@@ -0,0 +1,10 @@
+package hw1;
+
+public class hw1 {
+	public static void main(String[] args) {
+		System.out.println("I'am ½²®¦´­");
+		System.out.println("My nid is D0618439");
+		System.out.println("This is lab 1 hw");
+
+	}
+}
